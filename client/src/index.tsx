1a30baffebdd0421f21e885c421ec20193f029b6
kimba <skyqwn@gmail.com>
2024-05-20 21:09:33 +0900

    asd

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-
+//
 import { BrowserRouter } from "react-router-dom";
 
 import { CookiesProvider } from "react-cookie";
