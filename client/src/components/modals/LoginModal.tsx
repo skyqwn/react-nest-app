b613a7cf8eac4e5e7e9bd860c931d42d72f762d8
kimba <skyqwn@gmail.com>
2024-05-24 16:34:20 +0900

    env

--- client/src/components/modals/LoginModal.tsx
+++ client/src/components/modals/LoginModal.tsx
@@ -62,6 +62,7 @@ const LoginModal = () => {
   // 모달의 위치 계산
   // const top = `calc( ${scrollPosition}px)`;
   const top = scrollPosition + "px";
+  console.log(`${process.env.REACT_APP_GOOGEL_URL}`);
 
   const onValid: SubmitHandler<FieldValues> = async (data) => {
     if (loggedIn) {
@@ -134,14 +135,12 @@ const LoginModal = () => {
         <span className="text-blue-400">개인 정보 보호</span>
         정책을 이해했음을 인정합니다.
       </span>
-
       <Link to={`${process.env.REACT_APP_GOOGEL_URL}`}>
         <div className="w-full h-10 flex items-center justify-center border-2 rounded-2xl gap-2">
           <FcGoogle className="text-3xl" />
           <span className="font-semibold">구글로그인</span>
         </div>
       </Link>
-
       <div className="relative">
         <div className="absolute w-full border-t border-gray-300" />
         <div className="relative -top-3 text-center ">
