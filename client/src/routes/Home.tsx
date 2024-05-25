471740398399d3e797af084e7c39bdb8ed0436bd
kimba <skyqwn@gmail.com>
2024-05-25 13:11:37 +0900

    test

--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -3,6 +3,7 @@ import Layout from "../components/Layout";
 import CreatePosts from "../components/CreatePosts";
 
 export const Home = () => {
+  console.log(process.env.REACT_APP_GOOGLE_URL);
   return (
     <Layout>
       <div className="flex flex-col p-4 w-full">
